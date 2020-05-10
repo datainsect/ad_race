d64d3cab8caf0289adc05ee1a25111dd7c200da5
liuguilin <liuguilin2014@xiaochuankeji.cn>
2020-05-10 11:41:23 +0800

    inti

--- data_prepare.py
+++ data_prepare.py
@@ -1,11 +1,16 @@
-import pandas 
+import pandas  as pd
+
+project = '/home/tione/notebook/'
 
 ## 1.process train file
-user_time.csv = '/home/tione/work/ad_race/csvs/train/user_time.csv'
-user_creative_id = '/home/tione/work/ad_race/csvs/train/user_creative_id.csv'
-user_ad_id = '/home/tione/work/ad_race/csvs/train/user_ad_id.csv'
-user_advertiser_id = '/home/tione/work/ad_race/csvs/train/user_advertiser_id.csv'
-user_industry = '/home/tione/work/ad_race/csvs/train/user_industry.csv'
-user_product_id = '/home/tione/work/ad_race/csvs/train/user_product_id.csv'
-user_product_category = '/home/tione/work/ad_race/csvs/train/user_product_category.csv'
+user_time_path = project + 'train/user/user_time.csv'
+user_creative_id_path = project + 'train/user/user_creative_id.csv'
+user_ad_id_path = project + 'train/user/user_ad_id.csv'
+user_industry_path = project + 'train/user/user_industry.csv'
+user_advertiser_id_path = project + 'train/user/user_advertiser_id.csv'
+user_product_id_path = project + 'train/user/user_product_id.csv'
+user_product_category_path = project + 'train/user/user_product_category.csv'
+
+user_time_path = pd.read_csv('user_time_path')
+
 
--- env.sh
+++ env.sh
@@ -1,3 +1,1 @@
-conda create -n dnn python=3.6.5
-source activate dnn
 pip install -r requirement.txt
