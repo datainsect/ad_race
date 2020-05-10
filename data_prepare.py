1b16310a17e19a2621865b59bc558b0ea2764a98
liuguilin <liuguilin2014@xiaochuankeji.cn>
2020-05-10 11:16:27 +0800

    inti

---
+++ data_prepare.py
@@ -0,0 +1,11 @@
+import pandas 
+
+## 1.process train file
+user_time.csv = '/home/tione/work/ad_race/csvs/train/user_time.csv'
+user_creative_id = '/home/tione/work/ad_race/csvs/train/user_creative_id.csv'
+user_ad_id = '/home/tione/work/ad_race/csvs/train/user_ad_id.csv'
+user_advertiser_id = '/home/tione/work/ad_race/csvs/train/user_advertiser_id.csv'
+user_industry = '/home/tione/work/ad_race/csvs/train/user_industry.csv'
+user_product_id = '/home/tione/work/ad_race/csvs/train/user_product_id.csv'
+user_product_category = '/home/tione/work/ad_race/csvs/train/user_product_category.csv'
+
---
+++ env.sh
@@ -0,0 +1,3 @@
+conda create -n dnn python=3.6.5
+source activate dnn
+pip install -r requirement.txt
