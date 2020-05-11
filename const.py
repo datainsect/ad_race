1c2a44db0e59c1d89de9f5a283cb6a8c266821bf
liuguilin <liuguilin2014@xiaochuankeji.cn>
2020-05-11 23:47:17 +0800

    billing rank

--- const.py
+++ const.py
@@ -1,8 +1,10 @@
 
 
-test_ad = 'csvs/test/ad.csv'
-test_click_log = 'csvs/test/click_log.csv'
+test_ad_path = 'csvs/test/ad.csv'
+test_click_log_path = 'csvs/test/click_log.csv'
 
-train_ad = 'csvs/train/ad.csv'
-train_click_log = 'csvs/train/click_log.csv'
-train_user = 'csvs/train/user.csv'
+train_ad_path = 'csvs/train/ad.csv'
+train_click_log_path = 'csvs/train/click_log.csv'
+train_user_path = 'csvs/train/user.csv'
+
+raw_joined = 'csvs/raw_joined.csv'
