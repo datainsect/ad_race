aa1e38ebd08cec0fcfa9139fd341d15cdb497c1d
liuguilin <liuguilin2014@xiaochuankeji.cn>
2020-05-12 00:41:22 +0800

    billing rank

--- dnn/DeepFM.py
+++ dnn/DeepFM.py
@@ -54,7 +54,7 @@ class MyMeanPool(Layer):
 
 
 class DeepFM:
-    def __init__(self,cate_features,list_features,features_num_dict,k=10,list_k =10*20, optimizer=Adam(0.001)):
+    def __init__(self,cate_features,list_features,features_num_dict,k=10,list_k =10*40, optimizer=Adam(0.001)):
         self.cate_features = cate_features
         self.list_features = list_features
         self.features_num_dict = features_num_dict
