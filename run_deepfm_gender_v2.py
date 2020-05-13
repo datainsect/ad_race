26639087109b98d258f0ebef76a960735faa1f89
liuguilin <liuguilin2014@xiaochuankeji.cn>
2020-05-14 00:21:37 +0800

    v1

--- run_deepfm_gender_v2.py
+++ run_deepfm_gender_v2.py
@@ -113,7 +113,7 @@ print(time.strftime('%Y-%m-%d %H:%M:%S')+ "  :X_test list processed")
 
 ## 4. Define Model, train, predict and evaluate
 
-checkpoint = ModelCheckpoint('models/gender_v1.h5', save_weights_only=False, save_best_only=True)
+checkpoint = ModelCheckpoint('models/gender_v2.h5', save_weights_only=False, save_best_only=True)
 callbacks_list = [checkpoint]
 
 model = DeepFM(sparse_features, list_features,features_num_dict,k=10,list_k=100).model
@@ -121,4 +121,4 @@ model.compile("adam", "binary_crossentropy",metrics=['binary_crossentropy','acc'
 
 history = model.fit(model_input, y_train,batch_size=batch_size, epochs=epochs, verbose=1, shuffle=True,validation_data=(model_output,y_test),callbacks=callbacks_list)
 
-model.save('models/gender_v1_final.h5')
+model.save('models/gender_v2_final.h5')
