ac1ae515ec38327a83bfe6164046643cf7fbc620
liuguilin <liuguilin2014@xiaochuankeji.cn>
2020-05-14 00:37:52 +0800

    v1

--- run_data_prepare_test.py
+++ run_data_prepare_test.py
@@ -2,6 +2,8 @@ import pandas  as pd
 import numpy as np
 import math
 
+import time
+
 from const import *
 from utils import *
 
@@ -21,6 +23,7 @@ user_time = pd.read_csv(user_time_test_path)
 user_time[term] = user_time[term].apply(lambda x : list(map(lambda y : int(y%k),eval(str(x)))))
 # user_time[term]= user_time[term].astype(np.int16)
 # user_time = user_time[user_time['user_id']!=839368]
+print(time.strftime('%Y-%m-%d %H:%M:%S')+ "  :time made")
 
 #1.2 creative_id 2481135  4445718
 k = 26713
@@ -28,6 +31,7 @@ term = 'creative_id'
 user_creative_id = pd.read_csv(user_creative_id_test_path)
 user_creative_id[term] = user_creative_id[term].apply(lambda x : list(map(lambda y : int(y%k),eval(str(x)))))
 # user_creative_id[term] = user_creative_id[term].astype(np.int16)
+print(time.strftime('%Y-%m-%d %H:%M:%S')+ "  :creative_id made")
 
 #1.3 ad_id 2264190 3812200
 k = 26713
@@ -35,6 +39,7 @@ term = 'ad_id'
 user_ad_id = pd.read_csv(user_ad_id_test_path)
 user_ad_id[term] = user_ad_id[term].apply(lambda x : list(map(lambda y : int(y%k),eval(str(x)))))
 # user_ad_id[term] = user_ad_id[term].astype(np.int16)
+print(time.strftime('%Y-%m-%d %H:%M:%S')+ "  :ad_id made")
 
 
 #1.4 advertiser_id  52090 62965.0
@@ -43,6 +48,7 @@ term = 'advertiser_id'
 user_advertiser_id = pd.read_csv(user_advertiser_id_test_path)
 user_advertiser_id[term] = user_advertiser_id[term].apply(lambda x : list(map(lambda y : int(y%k),eval(str(x)))))
 # user_advertiser_id[term] = user_advertiser_id[term].astype(np.int16)
+print(time.strftime('%Y-%m-%d %H:%M:%S')+ "  :advertiser_id made")
 
 #1.5 industry 326 335.0 nan
 k = 336
@@ -50,6 +56,8 @@ term = 'industry'
 user_industry = pd.read_csv(user_industry_test_path)
 user_industry[term] = user_industry[term].apply(lambda x : list(map(lambda y : int(y%k),eval(rmnan(str(x))))))
 # user_industry[term] = user_industry[term].astype(np.int16)
+print(time.strftime('%Y-%m-%d %H:%M:%S')+ "  :industry made")
+
 
 #1.6 product_id 33273 44313.0 nan
 k = 26713
@@ -57,6 +65,8 @@ term = 'product_id'
 user_product_id = pd.read_csv(user_product_id_test_path)
 user_product_id[term] = user_product_id[term].apply(lambda x : list(map(lambda y : int(y%k),eval(rmnan(str(x))))))
 # user_product_id[term] = user_product_id[term].astype(np.int16)
+print(time.strftime('%Y-%m-%d %H:%M:%S')+ "  :product_id made")
+
 
 #1.7 product_category  18 18
 k = 19
@@ -64,6 +74,8 @@ term = 'product_category'
 user_product_category = pd.read_csv(user_product_category_test_path)
 user_product_category[term] = user_product_category[term].apply(lambda x : list(map(lambda y : int(y%k),eval(str(x)))))
 # user_product_category[term] = user_product_category[term].astype(np.int16)
+print(time.strftime('%Y-%m-%d %H:%M:%S')+ "  :product_category made")
+
 
 #1.8 join sequence and to_csv
 dfs = [user_time,user_creative_id,user_ad_id,user_advertiser_id,user_industry,user_product_id,user_product_category]
@@ -72,3 +84,4 @@ for i in range(1,len(dfs)):
     df = pd.merge(df,dfs[i],on='user_id')
 
 df.to_csv(project+'test/user/user_sequence.csv',index=False)
+print(time.strftime('%Y-%m-%d %H:%M:%S')+ "  : done")
